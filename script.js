b5bef478f4c477faf784ea09e3e949c3ed97b050
marvrod <marcelovvilla@gmail.com>
2022-08-17 10:24:20 -0500

    Add number+CE clicking and displaying

--- script.js
+++ script.js
@@ -13,3 +13,53 @@ function multiply(x, y){
 function divide(x, y){
     return x / y;
 }
+
+function operate(x, y, func){
+    return func(x, y);
+}
+
+// Const for display
+const display = document.querySelector("#display");
+
+
+
+// Mechanism for getting number values from the HTML
+var ancestor = document.getElementById("numbers");
+var descendents = ancestor.getElementsByTagName("*");
+
+var numberList = Array.from(descendents);
+
+
+function extractText(element){
+    return element.innerText;
+}
+
+function extractID(element){
+    return element.id;
+}
+var trueNumberList = numberList.map(extractText);
+
+
+
+let displayValue;
+
+// Mechanism for listening through the numbers in the HTML and storing the display value
+
+for (let i = 0; i < trueNumberList.length; i++) {
+
+    const numberRead = document.querySelector(`#${extractID(numberList[i])}`);
+   numberRead.addEventListener('click', function(){
+    display.textContent = `${display.textContent + numberRead.innerText}`;
+    displayValue = display.textContent;
+    console.log(displayValue);
+})
+}
+
+const erase = document.querySelector('#CE');
+erase.addEventListener('click', ()=> {
+    display.textContent = '';
+})
+// Variable for storing display value
+
+
+
