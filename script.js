77df064e5cdb54c76b3cedbbacc97e3ed085fd72
marvrod <marcelovvilla@gmail.com>
2022-08-17 12:35:57 -0500

    Add operator functionality, non-chained

--- script.js
+++ script.js
@@ -21,14 +21,10 @@ function operate(x, y, func){
 // Const for display
 const display = document.querySelector("#display");
 
-
-
 // Mechanism for getting number values from the HTML
 var ancestor = document.getElementById("numbers");
 var descendents = ancestor.getElementsByTagName("*");
-
 var numberList = Array.from(descendents);
-
 
 function extractText(element){
     return element.innerText;
@@ -39,27 +35,88 @@ function extractID(element){
 }
 var trueNumberList = numberList.map(extractText);
 
-
-
 let displayValue;
+let firstValue;
+let secondValue;
+let reset = true;
 
-// Mechanism for listening through the numbers in the HTML and storing the display value
+// Mechanism for listening through the numbers in the HTML and storing the display value.
 
 for (let i = 0; i < trueNumberList.length; i++) {
 
     const numberRead = document.querySelector(`#${extractID(numberList[i])}`);
-   numberRead.addEventListener('click', function(){
+    numberRead.addEventListener('click', function(){
+    pressed = false;
+    if (!operator){
     display.textContent = `${display.textContent + numberRead.innerText}`;
-    displayValue = display.textContent;
-    console.log(displayValue);
+
+    firstValue = parseInt(display.textContent);
+    }
+    else {
+    if (display.textContent == "+" || display.textContent == "-" || display.textContent == "*" 
+    || display.textContent == "/" || reset == true){    
+        display.textContent = '';
+        reset = false;
+    }
+    display.textContent = `${display.textContent + numberRead.innerText}`;
+    secondValue = parseInt(display.textContent);
+    }
+    console.log(secondValue);
 })
 }
-
+// CE button
 const erase = document.querySelector('#CE');
 erase.addEventListener('click', ()=> {
+    pressed = false;
     display.textContent = '';
+    firstValue = 0;
+    secondValue = 0;
+    operator = 0;
 })
-// Variable for storing display value
 
+let operator;
 
+// + button
+const plusButton = document.querySelector('#PLUS');
+plusButton.addEventListener('click', ()=> {
+    display.textContent = `+`;
+    operator = add;
+});
+
+// - button
+const minusButton = document.querySelector("#MINUS");
+minusButton.addEventListener('click', ()=> {
+    display.textContent = "-";
+    operator = substract;
+});
+
+// * button
+const multiplyButton =  document.querySelector("#MULTIPLY")
+multiplyButton.addEventListener('click', ()=> {
+    display.textContent = "*";
+    operator = multiply;
+});
+
+// / button
+const divideButton =  document.querySelector("#DIVIDE")
+divideButton.addEventListener('click', ()=> {
+    display.textContent = "/";
+    operator = divide;
+});
+
+// = button
+let pressed = false;
+
+const equalButton = document.querySelector("#EQUAL");
+equalButton.addEventListener('click', ()=> {
+    if (!pressed) {
+    let result = operate(firstValue, secondValue, operator);
+    display.textContent = result;
+    pressed = true;
+    firstValue = result;
+    secondValue = 0;
+
+    reset = true;
+    }
+})
 
